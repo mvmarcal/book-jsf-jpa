389ad93c815b288c411915911dfc956c5bcdf4c8
Matheus V. Marçal <mvmarcal@gmail.com>
2015-08-17 17:14:03 -0300

    Classe Automovel com getters and setters e metodos padrão

--- src/org/bookjspjpa/com/entity/Automovel.java
+++ src/org/bookjspjpa/com/entity/Automovel.java
@@ -5,5 +5,105 @@ public class Automovel {
 	
 	private Integer anoFabricacao;
 	private String modelo;
+	private Integer anoModelo;
+	private String marca;
+	private String observacoes;
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public Integer getAnoFabricacao() {
+		return anoFabricacao;
+	}
+	public void setAnoFabricacao(Integer anoFabricacao) {
+		this.anoFabricacao = anoFabricacao;
+	}
+	public String getModelo() {
+		return modelo;
+	}
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+	public Integer getAnoModelo() {
+		return anoModelo;
+	}
+	public void setAnoModelo(Integer anoModelo) {
+		this.anoModelo = anoModelo;
+	}
+	public String getMarca() {
+		return marca;
+	}
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+	public String getObservacoes() {
+		return observacoes;
+	}
+	public void setObservacoes(String observacoes) {
+		this.observacoes = observacoes;
+	}
+	@Override
+	public String toString() {
+		return "Automovel [id=" + id + ", anoFabricacao=" + anoFabricacao
+				+ ", modelo=" + modelo + ", anoModelo=" + anoModelo
+				+ ", marca=" + marca + ", observacoes=" + observacoes + "]";
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((anoFabricacao == null) ? 0 : anoFabricacao.hashCode());
+		result = prime * result
+				+ ((anoModelo == null) ? 0 : anoModelo.hashCode());
+		result = prime * result + (int) (id ^ (id >>> 32));
+		result = prime * result + ((marca == null) ? 0 : marca.hashCode());
+		result = prime * result + ((modelo == null) ? 0 : modelo.hashCode());
+		result = prime * result
+				+ ((observacoes == null) ? 0 : observacoes.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Automovel other = (Automovel) obj;
+		if (anoFabricacao == null) {
+			if (other.anoFabricacao != null)
+				return false;
+		} else if (!anoFabricacao.equals(other.anoFabricacao))
+			return false;
+		if (anoModelo == null) {
+			if (other.anoModelo != null)
+				return false;
+		} else if (!anoModelo.equals(other.anoModelo))
+			return false;
+		if (id != other.id)
+			return false;
+		if (marca == null) {
+			if (other.marca != null)
+				return false;
+		} else if (!marca.equals(other.marca))
+			return false;
+		if (modelo == null) {
+			if (other.modelo != null)
+				return false;
+		} else if (!modelo.equals(other.modelo))
+			return false;
+		if (observacoes == null) {
+			if (other.observacoes != null)
+				return false;
+		} else if (!observacoes.equals(other.observacoes))
+			return false;
+		return true;
+	}
+	
+	
 
 }
